591b2cd4607b4d074c04139a9690653f9f59c284
Simon Waldherr <git@simon.waldherr.eu>
2012-08-26 23:24:09 +0200

    add apple-touch-startup-image, but didn't work

--- index.php
+++ index.php
@@ -30,6 +30,8 @@ function getEpisodes($Podcast)
 	<link rel="stylesheet" href="http://cdn.shownot.es/css/style.css" type="text/css" />
 	<link rel="stylesheet" href="http://cdn.shownot.es/css/baf.css" type="text/css"  media="screen" />
 	<link rel="author" href="./humans.txt" />
+	<link rel="apple-touch-startup-image" href="http://cdn.shownot.es/img/iPhonePortrait.png" />
+	<link rel="apple-touch-startup-image" sizes="768x1004" href="http://cdn.shownot.es/img/iPadPortait.png" />
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 	<script src="http://google-code-prettify.googlecode.com/svn/trunk/src/prettify.js"></script>
 </head>
