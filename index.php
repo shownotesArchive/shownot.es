8b1c30e2ee8e96dc39899ae9fbd338dc2949eab7
Simon Waldherr <git@simon.waldherr.eu>
2012-08-27 23:45:57 +0200

    Pageviews are cached for 85000 seconds (approximately one day)

--- index.php
+++ index.php
@@ -1,26 +1,6 @@
-<?php
-function getEpisodes($Podcast, $count)
-	{
-		if ($handle = @scandir('./podcasts/'.$Podcast, 1))
-			{
-				foreach($handle as $file)
-					{
-						if ($file != "." && $file != "..")
-							{
-								$Episode = explode('.', $file);
-								echo '<li><a onclick="TINY.box.show({url:\'./podcasts/'.$Podcast.'/'.$file.'\'})">'.(2 < count($Episode) ? $Episode[1] : $Episode[0]).'</a></li>';
-								++$count;
-							}
-					}
-			}
-		else
-			{
-				echo "<li>Verzeichnis leer</li>";
-			}
-		return $count;
-	}
-//<li><a onclick="TINY.box.show({url:'./podcasts/ll/12_07_12.html'})">12.07.2012</a></li>
-?><!DOCTYPE html>
+<?php if(1345000000 < time()){
+Header( "HTTP/1.1 302 Found" );
+Header( "Location: http://shownot.es/update/" );} ?><!DOCTYPE html>
 <html lang="de"> 
 
 <head>
@@ -38,7 +18,10 @@ function getEpisodes($Podcast, $count)
 	<script src="http://google-code-prettify.googlecode.com/svn/trunk/src/prettify.js"></script>
 </head>
 
-<body>
+<body><!-- 
+zuletzt aktualisiert um: 1346103568
+Generierungsdauer: 0.0019018650054932 sec
+-->
 <div class="content">
 	<div class="header">
 		<div class="title"><a href="/"><img src="http://cdn.shownot.es/img/logo.png">Die Shownotes</a></div>
@@ -58,8 +41,7 @@ function getEpisodes($Podcast, $count)
 					<span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu">
-						<?php $i = getEpisodes('wrint', 0); ?>
-				</ul>
+						<li><a onclick="TINY.box.show({url:'./podcasts/wrint/99.WRINT-99.html'})">WRINT-99</a></li><li><a onclick="TINY.box.show({url:'./podcasts/wrint/95.WRINT-95.html'})">WRINT-95</a></li><li><a onclick="TINY.box.show({url:'./podcasts/wrint/92.WRINT-92.html'})">WRINT-92</a></li><li><a onclick="TINY.box.show({url:'./podcasts/wrint/83.WRINT-83.html'})">WRINT-83</a></li><li><a onclick="TINY.box.show({url:'./podcasts/wrint/78.WRINT-78.html'})">WRINT-78</a></li><li><a onclick="TINY.box.show({url:'./podcasts/wrint/70.WRINT-70.html'})">WRINT-70</a></li><li><a onclick="TINY.box.show({url:'./podcasts/wrint/68.WRINT-68.html'})">WRINT-68</a></li><li><a onclick="TINY.box.show({url:'./podcasts/wrint/56.WRINT-56.html'})">WRINT-56</a></li><li><a onclick="TINY.box.show({url:'./podcasts/wrint/53.WRINT-53.html'})">WRINT-53</a></li><li><a onclick="TINY.box.show({url:'./podcasts/wrint/49.WRINT-49.html'})">WRINT-49</a></li><li><a onclick="TINY.box.show({url:'./podcasts/wrint/46.WRINT-46.html'})">WRINT-46</a></li><li><a onclick="TINY.box.show({url:'./podcasts/wrint/41.WRINT-41.html'})">WRINT-41</a></li><li><a onclick="TINY.box.show({url:'./podcasts/wrint/38.WRINT-38.html'})">WRINT-38</a></li><li><a onclick="TINY.box.show({url:'./podcasts/wrint/34.WRINT-34.html'})">WRINT-34</a></li><li><a onclick="TINY.box.show({url:'./podcasts/wrint/33.WRINT-33.html'})">WRINT-33</a></li>				</ul>
 			</div>
 			<div class="g-button-group">
 				<a class="g-button" href="http://www.fritz.de/media/podcasts/sendungen/blue_moon.html">Blue&nbsp;Moon</a>
@@ -67,8 +49,7 @@ function getEpisodes($Podcast, $count)
 					<span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu">
-						<?php $i = getEpisodes('bm', $i); ?>
-				</ul>
+						<li><a onclick="TINY.box.show({url:'./podcasts/bm/31_07_12.html'})">31_07_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/29_05_12.html'})">29_05_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/28_03_12.html'})">28_03_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/27_06_2012.html'})">27_06_2012</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/27_06_12.html'})">27_06_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/27_04_12.html'})">27_04_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/27_03_12.html'})">27_03_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/26_07_12.html'})">26_07_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/26_06_2012.html'})">26_06_2012</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/26_06_12.html'})">26_06_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/24_07_12.html'})">24_07_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/24_04_12.html'})">24_04_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/22_05_12.html'})">22_05_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/21_08_12.html'})">21_08_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/21_06_2012.html'})">21_06_2012</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/21_06_12.html'})">21_06_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/20_3_12.html'})">20_3_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/19_07_12.html'})">19_07_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/19_06_12.html'})">19_06_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/19-latest.html'})">19-latest</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/17_07_12.html'})">17_07_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/17_04_12.html'})">17_04_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/16_08_12.html'})">16_08_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/15_05_2012.html'})">15_05_2012</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/15_05_12.html'})">15_05_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/14_08_12.html'})">14_08_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/12_07_12.html'})">12_07_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/12_06_2012.html'})">12_06_2012</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/12_06_12.html'})">12_06_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/11_07_12.html'})">11_07_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/10_07_12.html'})">10_07_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/10_04_12.html'})">10_04_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/08_05_12.html'})">08_05_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/07_08_12.html'})">07_08_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/06_06_12.html'})">06_06_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/05_07_12.html'})">05_07_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/03_04_12.html'})">03_04_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/03_02_2012.html'})">03_02_2012</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/03_02_12.html'})">03_02_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/01_08_12.html'})">01_08_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/bm/01-05-12.html'})">01-05-12</a></li>				</ul>
 			</div>
 			<div class="g-button-group">
 				<a class="g-button" href="http://blogs.hr-online.de/lateline/podcast/">LateLine</a>
@@ -76,8 +57,7 @@ function getEpisodes($Podcast, $count)
 					<span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu">
-						<?php $i = getEpisodes('ll', $i); ?>
-				</ul>
+						<li><a onclick="TINY.box.show({url:'./podcasts/ll/23_04_12.html'})">23_04_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/ll/12_07_12.html'})">12_07_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/ll/11_06_12.html'})">11_06_12</a></li><li><a onclick="TINY.box.show({url:'./podcasts/ll/05_07_12.html'})">05_07_12</a></li>				</ul>
 			</div>
 			<div class="g-button-group">
 				<a class="g-button" href="http://chaosradio.ccc.de/chaosradio.html">Chaosradio</a>
@@ -85,8 +65,7 @@ function getEpisodes($Podcast, $count)
 					<span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu">
-						<?php $i = getEpisodes('cr', $i); ?>
-				</ul>
+						<li><a onclick="TINY.box.show({url:'./podcasts/cr/180.html'})">180</a></li>				</ul>
 			</div>
 			<div class="g-button-group">
 				<a class="g-button" href="http://not-safe-for-work.de">Not Safe For Work</a>
@@ -94,8 +73,7 @@ function getEpisodes($Podcast, $count)
 					<span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu">
-						<?php $i = getEpisodes('nsfw', $i); ?>
-				</ul>
+						<li><a onclick="TINY.box.show({url:'./podcasts/nsfw/55.NSFW-55.html'})">NSFW-55</a></li><li><a onclick="TINY.box.show({url:'./podcasts/nsfw/54.NSFW-54.html'})">NSFW-54</a></li><li><a onclick="TINY.box.show({url:'./podcasts/nsfw/53.NSFW-53.html'})">NSFW-53</a></li><li><a onclick="TINY.box.show({url:'./podcasts/nsfw/52.NSFW-52.html'})">NSFW-52</a></li><li><a onclick="TINY.box.show({url:'./podcasts/nsfw/51.NSFW-51.html'})">NSFW-51</a></li><li><a onclick="TINY.box.show({url:'./podcasts/nsfw/50.NSFW-50.html'})">NSFW-50</a></li><li><a onclick="TINY.box.show({url:'./podcasts/nsfw/49.NSFW-49.html'})">NSFW-49</a></li><li><a onclick="TINY.box.show({url:'./podcasts/nsfw/48.NSFW-48.html'})">NSFW-48</a></li><li><a onclick="TINY.box.show({url:'./podcasts/nsfw/47.NSFW-47.html'})">NSFW-47</a></li><li><a onclick="TINY.box.show({url:'./podcasts/nsfw/46.NSFW-46.html'})">NSFW-46</a></li><li><a onclick="TINY.box.show({url:'./podcasts/nsfw/45.NSFW-45.html'})">NSFW-45</a></li><li><a onclick="TINY.box.show({url:'./podcasts/nsfw/44.NSFW-44.html'})">NSFW-44</a></li><li><a onclick="TINY.box.show({url:'./podcasts/nsfw/41.NSFW-41.html'})">NSFW-41</a></li>				</ul>
 			</div>
 			<div class="g-button-group">
 				<a class="g-button" href="http://einschlafen-podcast.de">Einschlafen&nbsp;Podcast</a>
@@ -103,8 +81,7 @@ function getEpisodes($Podcast, $count)
 					<span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu">
-						<?php $i = getEpisodes('ep', $i); ?>
-				</ul>
+						<li><a onclick="TINY.box.show({url:'./podcasts/ep/178.EP-178.html'})">EP-178</a></li><li><a onclick="TINY.box.show({url:'./podcasts/ep/177.EP-177.html'})">EP-177</a></li><li><a onclick="TINY.box.show({url:'./podcasts/ep/176.EP-176.html'})">EP-176</a></li><li><a onclick="TINY.box.show({url:'./podcasts/ep/169.EP-169.html'})">EP-169</a></li><li><a onclick="TINY.box.show({url:'./podcasts/ep/168.EP-168.html'})">EP-168</a></li><li><a onclick="TINY.box.show({url:'./podcasts/ep/167.EP-167.html'})">EP-167</a></li>				</ul>
 			</div>
 			<div class="g-button-group">
 				<a class="g-button" href="http://mobilemacs.de/">mobileMacs</a>
@@ -112,8 +89,7 @@ function getEpisodes($Podcast, $count)
 					<span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu">
-						<?php $i = getEpisodes('mm', $i); ?>
-				</ul>
+						<li><a onclick="TINY.box.show({url:'./podcasts/mm/94.MM-94.html'})">MM-94</a></li><li><a onclick="TINY.box.show({url:'./podcasts/mm/93.MM-93.html'})">MM-93</a></li><li><a onclick="TINY.box.show({url:'./podcasts/mm/92.MM-92.html'})">MM-92</a></li><li><a onclick="TINY.box.show({url:'./podcasts/mm/91.MM-91.html'})">MM-91</a></li><li><a onclick="TINY.box.show({url:'./podcasts/mm/90.MM-90.html'})">MM-90</a></li><li><a onclick="TINY.box.show({url:'./podcasts/mm/89.MM-89.html'})">MM-89</a></li><li><a onclick="TINY.box.show({url:'./podcasts/mm/88.MM-88.html'})">MM-88</a></li><li><a onclick="TINY.box.show({url:'./podcasts/mm/87.MM-87.html'})">MM-87</a></li><li><a onclick="TINY.box.show({url:'./podcasts/mm/86.MM-86.html'})">MM-86</a></li><li><a onclick="TINY.box.show({url:'./podcasts/mm/85.MM-85.html'})">MM-85</a></li><li><a onclick="TINY.box.show({url:'./podcasts/mm/84.MM-84.html'})">MM-84</a></li><li><a onclick="TINY.box.show({url:'./podcasts/mm/82.MM-82.html'})">MM-82</a></li><li><a onclick="TINY.box.show({url:'./podcasts/mm/81.MM-81.html'})">MM-81</a></li>				</ul>
 			</div>
 			<div class="g-button-group">
 				<a class="g-button" href="http://monoxyd.de/category/dieweisheit">Der&nbsp;Weisheit</a>
@@ -121,8 +97,7 @@ function getEpisodes($Podcast, $count)
 					<span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu">
-						<?php $i = getEpisodes('dw', $i); ?>
-				</ul>
+						<li><a onclick="TINY.box.show({url:'./podcasts/dw/19.html'})">19</a></li><li><a onclick="TINY.box.show({url:'./podcasts/dw/18.html'})">18</a></li><li><a onclick="TINY.box.show({url:'./podcasts/dw/17.html'})">17</a></li><li><a onclick="TINY.box.show({url:'./podcasts/dw/14.html'})">14</a></li><li><a onclick="TINY.box.show({url:'./podcasts/dw/019.html'})">019</a></li><li><a onclick="TINY.box.show({url:'./podcasts/dw/018.html'})">018</a></li><li><a onclick="TINY.box.show({url:'./podcasts/dw/017.html'})">017</a></li><li><a onclick="TINY.box.show({url:'./podcasts/dw/014.html'})">014</a></li>				</ul>
 			</div>
 			<div class="g-button-group">
 				<a class="g-button" href="http://www.jobscast.de">Jobscast</a>
@@ -130,8 +105,7 @@ function getEpisodes($Podcast, $count)
 					<span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu">
-						<?php $i = getEpisodes('jc', $i); ?>
-				</ul>
+						<li><a onclick="TINY.box.show({url:'./podcasts/jc/011.html'})">011</a></li><li><a onclick="TINY.box.show({url:'./podcasts/jc/009.html'})">009</a></li><li><a onclick="TINY.box.show({url:'./podcasts/jc/008.html'})">008</a></li><li><a onclick="TINY.box.show({url:'./podcasts/jc/007.html'})">007</a></li>				</ul>
 			</div>
 			<div class="g-button-group">
 				<a class="g-button" href="http://ponytime.net/">Ponytime</a>
@@ -139,10 +113,9 @@ function getEpisodes($Podcast, $count)
 					<span class="caret"></span>
 				</a>
 				<ul class="dropdown-menu">
-						<?php $i = getEpisodes('pt', $i); ?>
-				</ul>
+						<li><a onclick="TINY.box.show({url:'./podcasts/pt/019.html'})">019</a></li><li><a onclick="TINY.box.show({url:'./podcasts/pt/018.html'})">018</a></li>				</ul>
 			</div>
-			<p>Zu diesen Podcasts gibt es bei uns insgesamt <?php echo $i; ?> Shownote Eintr&auml;ge. <br>Die gesamte Liste der Shownotes ist im <a href="https://shownotes.piratenpad.de/ep/padlist/all-pads">Etherpad</a> zu finden.</p><br>
+			<p>Zu diesen Podcasts gibt es bei uns insgesamt 107 Shownote Eintr&auml;ge. <br>Die gesamte Liste der Shownotes ist im <a href="https://shownotes.piratenpad.de/ep/padlist/all-pads">Etherpad</a> zu finden.</p><br>
 		</div></div>
 
 		<hr />
---
+++ update/index.php
@@ -0,0 +1,206 @@
+<?php
+$starttime = microtime(1);
+ob_start();
+
+function getEpisodes($Podcast, $count)
+	{
+		if ($handle = @scandir('./../podcasts/'.$Podcast, 1))
+			{
+				foreach($handle as $file)
+					{
+						if ($file != "." && $file != "..")
+							{
+								$Episode = explode('.', $file);
+								echo '<li><a onclick="TINY.box.show({url:\'./podcasts/'.$Podcast.'/'.$file.'\'})">'.(2 < count($Episode) ? $Episode[1] : $Episode[0]).'</a></li>';
+								++$count;
+							}
+					}
+			}
+		else
+			{
+				echo "<li>Verzeichnis leer</li>";
+			}
+		return $count;
+	}
+//<li><a onclick="TINY.box.show({url:'./podcasts/ll/12_07_12.html'})">12.07.2012</a></li>
+?><!DOCTYPE html>
+<html lang="de"> 
+
+<head>
+	<meta charset="utf-8" />
+	<title>Die Shownotes</title>
+	<meta name="viewport" content="width=980" />  
+	<link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
+	<link rel="icon" type="image/x-icon" href="./favicon.ico" />
+	<link rel="stylesheet" href="http://cdn.shownot.es/css/style.css" type="text/css" />
+	<link rel="stylesheet" href="http://cdn.shownot.es/css/baf.css" type="text/css"  media="screen" />
+	<link rel="author" href="./humans.txt" />
+	<link rel="apple-touch-startup-image" href="http://cdn.shownot.es/img/iPhonePortrait.png" />
+	<link rel="apple-touch-startup-image" sizes="768x1004" href="http://cdn.shownot.es/img/iPadPortait.png" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="http://google-code-prettify.googlecode.com/svn/trunk/src/prettify.js"></script>
+</head>
+
+<body>
+<div class="content">
+	<div class="header">
+		<div class="title"><a href="/"><img src="http://cdn.shownot.es/img/logo.png">Die Shownotes</a></div>
+	</div>
+	<div class="box" id="main">
+		<div class="title">Hallo!</div>
+		<p style="margin-top: 1em;">
+			Wir sind eine Community, die Shownotes f&uuml;r verschiedene Podcast- und Radioformate live mitnotiert. Du befindest dich gerade auf unserer tempor&auml;ren &Uuml;bergangsseite. Wir planen momentan, eine eigene Software zu modifizieren und in Betrieb zu nehmen, um das Schreiben von Shownotes zu erleichtern. <a href="http://i.minus.com/jcoBKNlPtJ5Lp.png">Hier</a> dazu eine kleine Vorschau. Bis dahin erreicht ihr die Pads nach wie vor &uuml;ber <a href="https://shownotes.piratenpad.de/"><strong>shownotes.piratenpad.de</strong></a>.
+		</p><hr><br><div id="podcasts">
+		<p style="margin-top: 1em;">
+			Wir schreiben aktuell f&uuml;r folgende Podcasts mehr oder weniger regelm&auml;ßig die Shownotes:
+		</p>
+		<div style="margin-top: 1em;">
+			<div class="g-button-group">
+				<a class="g-button" href="http://www.wrint.de/">WRINT</a>
+				<a class="g-button dropdown-toggle" data-toggle="dropdown" href="#">
+					<span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+						<?php $i = getEpisodes('wrint', 0); ?>
+				</ul>
+			</div>
+			<div class="g-button-group">
+				<a class="g-button" href="http://www.fritz.de/media/podcasts/sendungen/blue_moon.html">Blue&nbsp;Moon</a>
+				<a class="g-button dropdown-toggle" data-toggle="dropdown" href="#">
+					<span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+						<?php $i = getEpisodes('bm', $i); ?>
+				</ul>
+			</div>
+			<div class="g-button-group">
+				<a class="g-button" href="http://blogs.hr-online.de/lateline/podcast/">LateLine</a>
+				<a class="g-button dropdown-toggle" data-toggle="dropdown" href="#">
+					<span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+						<?php $i = getEpisodes('ll', $i); ?>
+				</ul>
+			</div>
+			<div class="g-button-group">
+				<a class="g-button" href="http://chaosradio.ccc.de/chaosradio.html">Chaosradio</a>
+				<a class="g-button dropdown-toggle" data-toggle="dropdown" href="#">
+					<span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+						<?php $i = getEpisodes('cr', $i); ?>
+				</ul>
+			</div>
+			<div class="g-button-group">
+				<a class="g-button" href="http://not-safe-for-work.de">Not Safe For Work</a>
+				<a class="g-button dropdown-toggle" data-toggle="dropdown" href="#">
+					<span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+						<?php $i = getEpisodes('nsfw', $i); ?>
+				</ul>
+			</div>
+			<div class="g-button-group">
+				<a class="g-button" href="http://einschlafen-podcast.de">Einschlafen&nbsp;Podcast</a>
+				<a class="g-button dropdown-toggle" data-toggle="dropdown" href="#">
+					<span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+						<?php $i = getEpisodes('ep', $i); ?>
+				</ul>
+			</div>
+			<div class="g-button-group">
+				<a class="g-button" href="http://mobilemacs.de/">mobileMacs</a>
+				<a class="g-button dropdown-toggle" data-toggle="dropdown" href="#">
+					<span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+						<?php $i = getEpisodes('mm', $i); ?>
+				</ul>
+			</div>
+			<div class="g-button-group">
+				<a class="g-button" href="http://monoxyd.de/category/dieweisheit">Der&nbsp;Weisheit</a>
+				<a class="g-button dropdown-toggle" data-toggle="dropdown" href="#">
+					<span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+						<?php $i = getEpisodes('dw', $i); ?>
+				</ul>
+			</div>
+			<div class="g-button-group">
+				<a class="g-button" href="http://www.jobscast.de">Jobscast</a>
+				<a class="g-button dropdown-toggle" data-toggle="dropdown" href="#">
+					<span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+						<?php $i = getEpisodes('jc', $i); ?>
+				</ul>
+			</div>
+			<div class="g-button-group">
+				<a class="g-button" href="http://ponytime.net/">Ponytime</a>
+				<a class="g-button dropdown-toggle" data-toggle="dropdown" href="#">
+					<span class="caret"></span>
+				</a>
+				<ul class="dropdown-menu">
+						<?php $i = getEpisodes('pt', $i); ?>
+				</ul>
+			</div>
+			<p>Zu diesen Podcasts gibt es bei uns insgesamt <?php echo $i; ?> Shownote Eintr&auml;ge. <br>Die gesamte Liste der Shownotes ist im <a href="https://shownotes.piratenpad.de/ep/padlist/all-pads">Etherpad</a> zu finden.</p><br>
+		</div></div>
+
+		<hr />
+		<p>
+			Um unsere Vorhabungen zu finanzieren, sind wir nach wie vor auf eure Spenden angewiesen. Daher w&uuml;rde es uns freuen, wenn ihr uns ab und zu <a href="https://flattr.com/thing/713059/dieshownotes-on-Twitter">flattern</a> k&ouml;nntet. Mit <a href="http://superfav.com/">SuperFav</a> werden &uuml;brigens <a href="http://twitter.com/dieshownotes">alle Tweets</a> automatisch geflattert.
+		</p>
+		<p style="margin-top: 1em">Zus&auml;tzliche Informationen sind &uuml;ber unsere Twitter Accounts zu erhalten: <a href="http://twitter.com/dieshownotes">@DieShownotes</a>, <a href="http://twitter.com/quimoniz">@Quimoniz</a>, <a href="http://twitter.com/gurkitier">@Gurkitier</a>, <a href="http://twitter.com/kaeffchen_heinz">@kaeffchen_heinz</a>, <a href="http://twitter.com/simonwaldherr">@SimonWaldherr</a>.</p>
+		<p style="margin-top: 1em;">
+			Neben Twitter k&ouml;nnt ihr uns auch im IRC auf <a href="irc://irc.freenode.net/shownotes">freenode</a> erreichen. (<a href="http://webchat.freenode.net/?channels=shownotes">Webchat</a>)
+		</p>
+		<hr />
+		<p>Der Großteil der Entwicklung erfolgt auf <a href="https://github.com/">GitHub</a>. Jeder der mithelfen will, kann gerne Pull-Requests an die Repositorys <a href="https://github.com/SimonWaldherr/shownot.es">GitHub.com/SimonWaldherr/shownot.es</a>, <a href="https://github.com/mluto/ShowPad">GitHub.com/mluto/ShowPad</a> sowie <a href="https://github.com/mluto/etherpad-lite">GitHub.com/mluto/etherpad-lite</a> schicken.</p>
+		<hr />
+		<p>Wer Podcasts mag, sollte <a href="http://podpott.de/">Podpott</a> und <a href="http://hoersuppe.de/">die H&ouml;rsuppe</a> kennen.</p>
+	</div>
+
+	<div class="footer">&nbsp;<span>&copy; 2012 <a href="/">shownot.es</a></div>
+
+</div>
+<script type="text/javascript" src="http://cdn.shownot.es/js/bootstrap-dropdown.js"></script>
+<script type="text/javascript" src="http://cdn.shownot.es/js/tinybox.js"></script>
+</body>
+
+</html>
+<?php
+
+$inhalt = ob_get_contents();
+ob_end_clean();
+
+$generatetime = microtime(1)-$starttime;
+
+if (!empty($inhalt))
+{
+	$filename = './../index.php';
+	$inhalt = explode('<body>', $inhalt);
+	$inhalt = $inhalt[0].'<body><!-- '."\n".'zuletzt aktualisiert um: '.time()."\n".'Generierungsdauer: '.$generatetime.' sec'."\n".'-->'.$inhalt[1];
+	
+	$code = '<?php if('.(time()+85000).' < time()){'."\n".'Header( "HTTP/1.1 302 Found" );'."\n".'Header( "Location: http://shownot.es/update/" );} ?>';
+	
+	$inhalt = $code.$inhalt;
+	if (!$handle = fopen($filename, 'w'))
+		{
+			echo 'Cannot open file '.$filename;
+			exit;
+		}
+
+	if (fwrite($handle, $inhalt) === FALSE)
+		{
+			echo 'Cannot write to file '.$filename;
+			exit;
+		}
+
+	fclose($handle);
+	header("HTTP/1.1 301 Moved Permanently");
+	header("Location: http://shownot.es/");
+	header("Connection: close");
+}
+?>
