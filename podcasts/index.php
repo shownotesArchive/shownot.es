ca68b9bfbb071eeab1cfdd9d8ac1c0b992ed0217
Simon Waldherr <git@simon.waldherr.eu>
2012-08-29 15:26:09 +0200

    Added functionality to open episode files with css

---
+++ podcasts/index.php
@@ -0,0 +1,97 @@
+<?php
+function getEpisodes($count)
+  {
+    if ($handle = @scandir('./'))
+      {
+        foreach($handle as $folder)
+          {
+            if ($folder != "." && $folder != ".." && $folder != 'index.php' && $folder != '.htaccess')
+              {
+                $handle2 = @scandir('./'.$folder, 1);
+                echo '<h2>'.$folder.'</h2><ol>';
+                foreach($handle2 as $file)
+                  {
+                    if ($file != "." && $file != ".." && $file != 'index.php')
+                      {
+                        $Episode = explode('.', $file);
+                        echo '<li><a href="./sn/'.$folder.'/'.$file.'">'.(2 < count($Episode) ? $Episode[1] : $Episode[0]).'</a></li>';
+                        ++$count;
+                      }
+                  }
+                echo '</ol>'."\n";
+              }
+          }
+      }
+    return $count;
+  }
+
+
+
+
+
+?><!DOCTYPE html>
+<html lang="de"> 
+
+<head>
+  <meta charset="utf-8" />
+  <title>Die Shownotes</title>
+  <meta name="viewport" content="width=980" />  
+  <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
+  <link rel="icon" type="image/x-icon" href="./favicon.ico" />
+  <link rel="stylesheet" href="http://cdn.shownot.es/css/style.css" type="text/css" />
+  <link rel="stylesheet" href="http://cdn.shownot.es/css/baf.css" type="text/css"  media="screen" />
+  <link rel="author" href="./humans.txt" />
+  <link rel="apple-touch-startup-image" href="http://cdn.shownot.es/img/iPhonePortrait.png" />
+  <link rel="apple-touch-startup-image" sizes="768x1004" href="http://cdn.shownot.es/img/iPadPortait.png" />
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+  <script src="http://google-code-prettify.googlecode.com/svn/trunk/src/prettify.js"></script>
+  <style>
+    h1 {
+      font-size: larger;
+      font-weight: bolder;
+    }
+    
+    h2 {
+      font-size: large;
+      margin: 15px;
+    }
+    
+    ol {
+    list-style: none;
+    margin-left: 45px;
+    }
+  </style>
+</head>
+
+<body>
+<div class="content">
+  <div class="header">
+    <div class="title"><a href="http://shownot.es/"><img src="http://cdn.shownot.es/img/logo.png">Die Shownotes</a></div>
+  </div>
+  <div class="box" id="main">
+    
+    <?php 
+    
+    if($_GET['podcast'] != '')
+      {
+        echo '<h2><a href="./../../">zur&uuml;ck zur &Uuml;bersicht</a></h2>';
+        include($_GET['podcast']);
+      }
+    else
+      {
+        echo '<h1>Dies ist eine Übersicht der Shownotes</h1><br>';
+        getEpisodes(1); 
+      }
+    
+    
+    ?>
+  </div>
+
+  <div class="footer">&nbsp;<span>&copy; 2012 <a href="/">shownot.es</a></div>
+
+</div>
+<script type="text/javascript" src="http://cdn.shownot.es/js/bootstrap-dropdown.js"></script>
+<script type="text/javascript" src="http://cdn.shownot.es/js/tinybox.js"></script>
+</body>
+
+</html>
