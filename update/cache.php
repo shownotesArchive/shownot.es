877f6f0ab851be6ece3d2cffeda050f25d80788f
Simon Waldherr <git@simon.waldherr.eu>
2012-09-28 21:08:41 +0200

    tinybox.js is now included as submodule, cache update

--- update/cache.php
+++ update/cache.php
@@ -11,7 +11,6 @@ function getEpisodes($Podcast, $count)
           {
             if ($file != "." && $file != "..")
               {
-                
                 $Episode = explode('.', $file);
                 $link = 'http://shownot.es/'.$Podcast.'/'.$Episode[0];
                 echo '<li><a onclick="TINY.box.show({url:\'./podcasts/'.$Podcast.'/'.$file.'\'}); return false" href="'.$link.'">'.(2 < count($Episode) ? $Episode[1] : $Episode[0]).'</a></li>';
@@ -28,7 +27,6 @@ function getEpisodes($Podcast, $count)
 
 ?><!DOCTYPE html>
 <html lang="de"> 
-
 <head>
   <meta charset="utf-8" />
   <title>Die Shownotes</title>
@@ -36,47 +34,13 @@ function getEpisodes($Podcast, $count)
   <meta name="apple-mobile-web-app-capable" content="yes" />  
   <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
   <link rel="icon" type="image/x-icon" href="./favicon.ico" />
+  <link rel="stylesheet" href="http://cdn.shownot.es/baf/css/baf.min.css?v=004" type="text/css"  media="screen" />
   <link rel="stylesheet" href="http://cdn.shownot.es/css/style.min.css?v=004" type="text/css" />
-  <link rel="stylesheet" href="http://cdn.shownot.es/css/baf/css/baf.min.css?v=004" type="text/css"  media="screen" />
   <link rel="stylesheet" href="http://cdn.shownot.es/css/anycast.min.css?v=004" type="text/css"  media="screen" />
   <link rel="apple-touch-startup-image" href="http://cdn.shownot.es/img/iPhonePortrait.png" />
   <link rel="apple-touch-startup-image" sizes="768x1004" href="http://cdn.shownot.es/img/iPadPortait.png" />
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-  <script src="http://google-code-prettify.googlecode.com/svn/trunk/src/prettify.js"></script>
-  <style>
-    iframe{
-      height: 1px;
-      width: 1px;
-      visibility: hidden;
-    }
-    .baf{
-      min-width: 70px;
-      margin-top: 10px;
-    }
-    .dropdown-menu{
-      right: 0;
-      min-width: 160px;
-      max-width: 200px;
-    }
-    .dropdown-menu li a{
-      padding: 5px 20px 5px 20px;
-    }
-  </style>
-  <script type="text/javascript">
-  
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-34667234-1']);
-    _gaq.push(['_trackPageview']);
-  
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = 'http://statistik.simon.waldherr.eu/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-  
-  </script>
 </head>
-
 <body>
 <div class="content">
   <div class="header">
@@ -246,11 +210,22 @@ function getEpisodes($Podcast, $count)
 <div class="footer">&nbsp;<span>&copy; 2012 <a href="/">shownot.es</a>
 </div>
 </div>
+<script type="text/javascript" src="http://cdn.shownot.es/baf/js/bootstrap-dropdown.js"></script>
+<script type="text/javascript" src="http://cdn.shownot.es/tinybox/tinybox.js"></script>
+<script type="text/javascript">
 
-<script type="text/javascript" src="http://cdn.shownot.es/js/bootstrap-dropdown.js"></script>
-<script type="text/javascript" src="http://cdn.shownot.es/js/tinybox.js"></script>
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-34667234-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'http://statistik.simon.waldherr.eu/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
-
 </html><?php
 
 $inhalt = ob_get_contents();
