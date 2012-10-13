99ae26ebf6afe6664cc13c2258c8cb08648bdf66
Simon Waldherr <git@simon.waldherr.eu>
2012-10-13 23:43:48 +0200

    mm/10 changed to osf-parsed version, psychotalk/6 osf-parsed added

--- podcasts/index.php
+++ podcasts/index.php
@@ -183,6 +183,19 @@ if(($podcast != '')&&($_GET['clear'] == 'true'))
 </div>
 <script type="text/javascript" src="http://cdn.shownot.es/js/bootstrap-dropdown.js"></script>
 <script type="text/javascript" src="http://cdn.shownot.es/js/tinybox.js"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-34667234-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'http://statistik.simon.waldherr.eu/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 
 </html>
