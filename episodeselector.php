5e2cff8e2490a3f9e31c5f527234726ab0feb346
Simon Waldherr <git@simon.waldherr.eu>
2012-12-08 19:59:00 +0100

    episodeselector code cleaned

--- episodeselector.php
+++ episodeselector.php
@@ -9,80 +9,54 @@ function insertselector()
     $next = '';
     $info = explode('/', $_GET['podcast']);
     $printit = '';
-    
-
-
-
-  if ($handle = @scandir('./'.$info[1].'/', 1))
-  {
-    foreach($handle as $file)
+    if ($handle = @scandir('./'.$info[1].'/', 1))
       {
-        if ($file != "." && $file != "..")
+        foreach($handle as $file)
           {
-            $Episode = explode('.', $file);
-            if($Episode[2] != '')
+            if ($file != "." && $file != "..")
               {
-                if(($count == 0)&&($Episode[0] == $info[2]))
+                $Episode = explode('.', $file);
+                if($Episode[2] != '')
                   {
-                    $last = true;
+                    if(($count == 0)&&($Episode[0] == $info[2]))
+                      {
+                        $last = true;
+                        
+                      }
                     
+                    if($Episode[0] == $info[2])
+                      {
+                        $count2 = $count;
+                      }
+                    
+                    $files[$count] = ltrim($Episode[0], '0 \t\n\r');
+                    $link = './'.ltrim($Episode[0], '0 \t\n\r');
+                    $printit .= '<li><a ';
+                    if($info[2] == ltrim($Episode[0], '0 \t\n\r'))
+                      {
+                        $printit .= 'id="selected" ';
+                      }
+                    $printit .= 'href="'.$link.'">Episode '.ltrim($Episode[0], '0 \t\n\r').'</a></li>';
+                    ++$count;
                   }
-                elseif(!$last)
-                  {
-                    $next = $Episode[0];
-                  }
-                
-                if($Episode[0] == $info[2])
-                  {
-                    $count2 = $count;
-                  }
-                
-                if($last && $prev == '')
-                  {
-                    $prev = $Episode[0];
-                  }
-                
-                $files[$count] = ltrim($Episode[0], '0 \t\n\r');
-                $link = './'.ltrim($Episode[0], '0 \t\n\r');
-                $printit .= '<li><a ';
-                if($info[2] == ltrim($Episode[0], '0 \t\n\r'))
-                  {
-                    $printit .= 'id="selected" ';
-                  }
-                $printit .= 'href="'.$link.'">Episode '.ltrim($Episode[0], '0 \t\n\r').'</a></li>';
-                ++$count;
               }
           }
       }
-    if(!$last)
+  
+    echo '<div class="baf-group-x1"><a class="baf grey prev" href="http://shownot.es/'.$info[1].'/'.$files[$count2+1].'"><span class="baf-icomoon" aria-hidden="true" data-icon="&#xe0c2;"></span></a><div class="baf-group"><a class="baf dropdown-toggle w80">Episode '.$files[$count2].'<span class="caret"></span></a><ul class="dropdown-menu">';
+    
+    echo $printit;
+    
+    echo '</ul></div><a class="baf grey next"';
+    if($last)
       {
-        //return $count;
+        echo ' disabled="true"';
       }
+    else
+      {
+        echo ' href="http://shownot.es/'.$info[1].'/'.$files[$count2-1].'"';
+      }
+    echo ' ><span class="baf-icomoon" aria-hidden="true" data-icon="&#xe0c0;"></span></a></div>';
   }
-  
-  echo '<div class="baf-group-x1">
-  <a class="baf grey prev" href="http://shownot.es/'.$info[1].'/'.$files[$count2+1].'"><span class="baf-icomoon" aria-hidden="true" data-icon="&#xe0c2;"></span></a>
-  <div class="baf-group">
-    <a class="baf dropdown-toggle w80">
-      Episode '.$files[$count2].'
-      <span class="caret"></span>
-    </a>
-    <ul class="dropdown-menu">';
-  
-  echo $printit;
-  
-  echo '</ul>
-</div><a class="baf grey next"';
-  if($last)
-    {
-      echo ' disabled="true"';
-    }
-  else
-    {
-      echo ' href="http://shownot.es/'.$info[1].'/'.$files[$count2-1].'"';
-    }
-  echo ' ><span class="baf-icomoon" aria-hidden="true" data-icon="&#xe0c0;"></span></a>
-</div>';
-}
 
 ?>
