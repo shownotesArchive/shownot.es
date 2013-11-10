a77bd87b5348c0b00b9755dc2b6582e44055d616
Simon Waldherr <git@simon.waldherr.eu>
2013-11-10 02:04:47 +0100

    OSF php submodule parse Dates

--- archive/buildtable.php
+++ archive/buildtable.php
@@ -2,35 +2,90 @@
 
 error_reporting(E_ALL);
 
-include_once ('./osf/osf.php');
-include_once ('./osf/parse.php');
+include_once ('./OSFphp/osf.php');
+include_once ('./OSFphp/parse.php');
 include_once ('./easySQL/easysql_sqlite.php');
 
+$sql = './archive.sqlite3';
 $dir = './showpad/';
 $dh  = scandir($dir);
 $i   = 0;
 
+date_default_timezone_set('Europe/Berlin');
+
+function parseUglyDate($str) {
+  switch (strlen($str)) {
+  case 8:
+    if(substr($str,4,4)+0 < 2020 && substr($str,4,4)+0 > 2000 && substr($str,0,2)+0 < 32 && substr($str,2,2)+0 < 13) {
+      return strtotime(substr($str,0,2).'.'.substr($str,2,2).'.'.substr($str,4,4));
+    } else if(substr($str,0,4)+0 < 2020 && substr($str,0,4)+0 > 2000) {
+      return strtotime(substr($str,6,2).'.'.substr($str,4,2).'.'.substr($str,0,4));
+    } else {
+      return false;
+    }
+    break;
+  case 6:
+    if(substr($str,4,2)+0 < 20 && substr($str,4,2)+0 > 0 && substr($str,0,2)+0 < 32 && substr($str,2,2)+0 < 13) {
+      return strtotime(substr($str,0,2).'.'.substr($str,2,2).'.20'.substr($str,4,2));
+    } else {
+      return false;
+    }
+    break;
+  case 5:
+    if(substr($str,3,2)+0 < 20 && substr($str,3,2)+0 > 0 && substr($str,0,2)+0 < 32 && substr($str,2,1)+0 > 0) {
+      return strtotime(substr($str,0,2).'.'.substr($str,2,1).'.20'.substr($str,3,2));
+    } else if(substr($str,3,2)+0 < 20 && substr($str,3,2)+0 > 0 && substr($str,0,2)+0 < 32 && substr($str,0,2)+0 > 0 && substr($str,2,1)+0 > 0) {
+      return strtotime(substr($str,0,2).'.'.substr($str,2,1).'.20'.substr($str,3,2));
+    } else if(substr($str,3,2)+0 < 20 && substr($str,3,2)+0 > 0 && substr($str,1,2)+0 < 32 && substr($str,1,2)+0 > 0) {
+      return strtotime(substr($str,0,1).'.'.substr($str,1,2).'.20'.substr($str,3,2));
+    } else {
+      return false;
+    }
+    break;
+  case 4:
+    if(substr($str,2,2)+0 < 20 && substr($str,2,2)+0 > 0) {
+      return strtotime(substr($str,0,1).'.'.substr($str,1,1).'.20'.substr($str,2,2));
+    } else {
+      return false;
+    }
+    break;
+  default:
+    return false;
+    break;
+  }
+  return false;
+}
+
 foreach($dh as $file) {
+  ini_set('max_execution_time', 120);
   if(is_file('./showpad/'.$file)) {
     $episode[$i]['file']['name'] = $file;
     $episode[$i]['file']['hash'] = hash_file("md5", './showpad/'.$file);
     $episode[$i]['file']['size'] = filesize('./showpad/'.$file);
-    
-    $select[0] = './archive.sqlite3';
+
+    $select[0] = $sql;
     $select[1] = 'episodes';
     $select['filename'] = $file;
 
     $returnarray = easysql_sqlite_select($select, 1);
     if($episode[$i]['file']['hash'] != $returnarray[0]['filehash']) {
       $json = json_decode(file_get_contents('./showpad/'.$file));
-      $parsed = parseWrapper($json->text);
+      $parsed = parserWrapper($json->text);
       $episode[$i]['podcast'] = trim($parsed['podcast'], " \t\n\r\0\x0B-");
       $episode[$i]['episode'] = ltrim(trim($parsed['episode'], " \t\n\r\0\x0B-"), "0");
+      if(preg_match('/\d{2}\.\d{2}\.\d{4}/', $parsed['episode'], $match)) {
+        $episode[$i]['episode'] = $match[0];
+      } else if($episode[$i]['episode']+0 > 1110) {
+        $date = parseUglyDate($episode[$i]['episode']);
+        if($date != false) {
+          $episode[$i]['episode'] = date("d.m.Y", $date);
+        }
+      }
       $episode[$i]['shownoter'] = $parsed['shownoter'];
       $episode[$i]['podcaster'] = $parsed['podcaster'];
       $episode[$i]['episodetime'] = $parsed['episodetime'];
-      
-      $insert[0] = './archive.sqlite3';
+
+      $insert[0] = $sql;
       $insert[1] = 'episodes';
       $insert['filename'] = $file;
       $insert['podcast'] = $episode[$i]['podcast'];
@@ -38,9 +93,8 @@ foreach($dh as $file) {
       $insert['filehash'] = $episode[$i]['file']['hash'];
       $insert['episodetime'] = $episode[$i]['episodetime'];
       $insert['filetime'] = time();
-      
-      $rowid = easysql_sqlite_insert($insert);
-      #echo $rowid;
+
+      easysql_sqlite_insert($insert);
     }
     $i++;
   }
